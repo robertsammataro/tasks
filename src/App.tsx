b1b11ae3e6b242952b948bc82c607024fc0541ef
Robert Sammataro <robbysam@udel.edu>
2022-02-11 14:27:35 -0500

    Completed Tasks for Task 3

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Button, Col, Container, Row } from "react-bootstrap";
 import "./App.css";
 
 function App(): JSX.Element {
@@ -11,6 +12,53 @@ function App(): JSX.Element {
                 Hello World! Edit <code>src/App.tsx</code> and save. This page
                 will automatically reload.
             </p>
+            <>
+                <h1>This is a header!</h1>
+                <img
+                    src="https://i.imgur.com/86pnF0S.jpg"
+                    alt="Robby at Epcot 2022"
+                />
+                <ul>
+                    <li>Hey here is my first bullet point</li>
+                    <li>Oh look here is another one</li>
+                    <li>Okay this is my last box.</li>
+                </ul>
+                <Button onClick={() => console.log("Hello World!")}>
+                    Log Hello World
+                </Button>
+                <Container>
+                    <Row>
+                        <Col>
+                            <div
+                                style={{
+                                    border: "1px solid red",
+                                    padding: "4px",
+                                    width: 250,
+                                    height: 50,
+                                    backgroundColor: "red",
+                                    color: "white"
+                                }}
+                            >
+                                This is my text!
+                            </div>
+                        </Col>
+                        <Col>
+                            <div
+                                style={{
+                                    border: "1px solid red",
+                                    padding: "4px",
+                                    width: 250,
+                                    height: 50,
+                                    backgroundColor: "red",
+                                    color: "white"
+                                }}
+                            >
+                                This is my text!
+                            </div>
+                        </Col>
+                    </Row>
+                </Container>
+            </>
         </div>
     );
 }
