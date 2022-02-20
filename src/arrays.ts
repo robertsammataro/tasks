bc5ca82d4a0a59818c3700557dcc099b543ec898
Robert Sammataro <rsammataro1126@gmail.com>
2022-02-19 21:32:25 -0500

    Finished tripleNumbers()

--- src/arrays.ts
+++ src/arrays.ts
@@ -13,7 +13,8 @@ export function bookEndList(numbers: number[]): number[] {
  * number has been tripled (multiplied by 3).
  */
 export function tripleNumbers(numbers: number[]): number[] {
-    return numbers;
+    const tripled_numbers = numbers.map((value: number): number => value * 3);
+    return tripled_numbers;
 }
 
 /**
