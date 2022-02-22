7e74486d03b16f879580858dff88725b9a376fc2
Robert Sammataro <rsammataro1126@gmail.com>
2022-02-21 20:17:34 -0500

    Completed publishAll()

--- src/nested.ts
+++ src/nested.ts
@@ -150,7 +150,10 @@ export function makeAnswers(questions: Question[]): Answer[] {
  * each question is now published, regardless of its previous published status.
  */
 export function publishAll(questions: Question[]): Question[] {
-    return [];
+    const publishedQuestions = questions.map(
+        (question: Question): Question => ({ ...question, published: true })
+    );
+    return publishedQuestions;
 }
 
 /***
