7ba1b0f16849665a3e62f921b764aefef5ec0410
Robert Sammataro <robbysam@udel.edu>
2022-02-28 21:15:48 -0500

    Completed StartAttempt()

--- src/components/StartAttempt.tsx
+++ src/components/StartAttempt.tsx
@@ -2,5 +2,42 @@ import React, { useState } from "react";
 import { Button } from "react-bootstrap";
 
 export function StartAttempt(): JSX.Element {
-    return <div>Start Attempt</div>;
+    const [inProgress, updateInProgress] = useState<boolean>(false);
+    const [remainingAttempts, updateRemainingAttempts] = useState<number>(4);
+
+    function startNewAttempt(): void {
+        updateInProgress(!inProgress);
+        updateRemainingAttempts(remainingAttempts - 1);
+    }
+
+    function stopAttempt(): void {
+        updateInProgress(!inProgress);
+    }
+
+    function mulligan(): void {
+        updateRemainingAttempts(remainingAttempts + 1);
+    }
+
+    return (
+        <div>
+            <p>
+                <Button
+                    onClick={startNewAttempt}
+                    disabled={inProgress || remainingAttempts === 0}
+                >
+                    Start Quiz
+                </Button>
+            </p>
+            <p>
+                <Button onClick={stopAttempt} disabled={!inProgress}>
+                    Stop Quiz
+                </Button>
+            </p>
+            <p>
+                <Button onClick={mulligan} disabled={inProgress}>
+                    Mulligan
+                </Button>
+            </p>
+        </div>
+    );
 }
