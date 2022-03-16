9b2c4b6024747be28227fa3c56ecc58b4c8532eb
Robert Sammataro <robbysam@udel.edu>
2022-03-16 18:55:17 -0400

    Begin Task 11

--- src/App.tsx
+++ src/App.tsx
@@ -16,6 +16,7 @@ import { EditMode } from "./form-components/EditMode";
 import { MultipleChoiceQuestion } from "./form-components/MultipleChoiceQuestion";
 import { ChangeColor } from "./form-components/ChangeColor";
 import { Quizzer } from "./quizzer/Quizzer";
+import { QuizResponse } from "./quizzer/QuizResponse";
 
 function App(): JSX.Element {
     return (
@@ -24,39 +25,8 @@ function App(): JSX.Element {
                 UD CISC275 with React Hooks and TypeScript - Robby Sammataro
             </header>
             <Quizzer></Quizzer>
+            <QuizResponse></QuizResponse>
             <hr></hr>
-            <CheckAnswer expectedAnswer="42"></CheckAnswer>
-            <hr></hr>
-            <GiveAttempts></GiveAttempts>
-            <hr></hr>
-            <EditMode></EditMode>
-            <hr></hr>
-            <ChangeColor></ChangeColor>
-            <hr></hr>
-            <MultipleChoiceQuestion
-                options={["a", "b", "c"]}
-                expectedAnswer="b"
-            ></MultipleChoiceQuestion>
-            <hr></hr>
-            {<DoubleHalf></DoubleHalf>}
-            <hr></hr>
-            <ChooseTeam></ChooseTeam>
-            <hr></hr>
-            <ColoredBox></ColoredBox>
-            <hr></hr>
-            <ShoveBox></ShoveBox>
-            <hr></hr>
-            <Counter></Counter>
-            <hr />
-            <RevealAnswer></RevealAnswer>
-            <hr />
-            <StartAttempt></StartAttempt>
-            <hr />
-            <TwoDice></TwoDice>
-            <hr />
-            <ChangeType></ChangeType>
-            <hr />
-            <CycleHoliday></CycleHoliday>
         </div>
     );
 }
---
+++ src/quizzer/QuizResponse.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export function QuizResponse(): JSX.Element {
+    return <h3>QuizResponse Object</h3>;
+}
